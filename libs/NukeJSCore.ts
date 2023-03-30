99a8864007d2df5716f9740e21b55c874d32620e
Hoang lee <hoang.le.tn91@gmail.com>
2023-03-30 16:26:45 +0700

    Release NukejsCore for beta 1.2

--- libs/NukeJSCore.ts
+++ libs/NukeJSCore.ts
@@ -2,23 +2,51 @@ interface NukeJSCoreInterface {
     hooks : {
         useState : Function
     }
-    doom : NukeJSDoom
+    doom : NukeJSDoom;
+    clientCallback: clientCallback
+}
+
+class clientCallback {
+    private makeKey(){
+        let key = "";
+        const array = ["N","U","K","E","J","S"];
+        for(let i=0;i<10;i++){
+            key += array[Math.floor(Math.random()*5)];
+        }
+        return key;
+    }
+    create(callback: Function) {
+        let key = "";
+        try {
+            let _window:any = window;
+            if(!_window.NukeJSCore) {
+                _window.NukeJSCore = new NukeJSCoreStructDefine;
+            }
+            key = 'CALLBACK_' + this.makeKey();
+            _window.NukeJSCore.callback[key] = callback;
+            window = _window;
+        }catch(e) {
+        }
+        return `NukeJSCore.callback.${key}()`;
+    }
+}
+class NukeJSCoreStructDefine {
+    status : any = {}
+    callback: any = {}
 }
 class NukeJSCoreUseState {
     constructor(private key:string="",private value:string="") {
         let _window : any = window;
-        if(_window.NukeJSCore.status[this.key]) {
+        if(_window.NukeJSCore && _window.NukeJSCore.status && _window.NukeJSCore.status[this.key]) {
             this.value = _window.NukeJSCore.status[this.key];
-        }
+        } 
     }
     set(value:any = "") {
         this.value = value;
         try {
             let _window:any = window;
             if(!_window.NukeJSCore) {
-                _window.NukeJSCore = {
-                    status : {}
-                };
+                _window.NukeJSCore = new NukeJSCoreStructDefine;
             }
             _window.NukeJSCore.status[this.key] =  this.value;
             _window.nukepage.render();
@@ -32,7 +60,21 @@ class NukeJSCoreUseState {
     }
 }
 class NukeJSDoom extends Document {
-
+    constructor(){
+        super();
+        
+    }
+    build(rootId:string = "",html : string=""){
+        this.add(rootId,html);
+    }
+    private add(rootId:string = "",html : string=""){
+        if(document.getElementById(rootId)) {
+            let root = window.document.getElementById(rootId);
+            if(root !== null) {
+                root.innerHTML = html;
+            }
+        }
+    }
 }
 class NukeJSAdapter implements NukeJSCoreInterface {
     hooks : {
@@ -43,6 +85,7 @@ class NukeJSAdapter implements NukeJSCoreInterface {
         }
     }
     doom: NukeJSDoom = new NukeJSDoom;
+    clientCallback: clientCallback = new clientCallback;
 }
 
 export default class _NukeJSCore extends NukeJSAdapter {}
