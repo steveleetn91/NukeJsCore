f21ec0b4f78201589d43e7337814573e863c670b
Hoang lee <hoang.le.tn91@gmail.com>
2023-03-31 21:26:56 +0700

    Update core

--- libs/NukeJSCore.ts
+++ libs/NukeJSCore.ts
@@ -1,91 +1,92 @@
 interface NukeJSCoreInterface {
-    hooks : {
-        useState : Function
+    hooks: {
+        useState: Function
     }
-    doom : NukeJSDoom;
+    doom: NukeJSDoom;
     clientCallback: clientCallback
 }
 
 class clientCallback {
-    private makeKey(){
+    private makeKey() {
         let key = "";
-        const array = ["N","U","K","E","J","S"];
-        for(let i=0;i<10;i++){
-            key += array[Math.floor(Math.random()*5)];
+        const array = ["N", "U", "K", "E", "J", "S","P","O","I","M","Q","W"];
+        for (let i = 0; i < 16; i++) {
+            key += array[Math.floor(Math.random() * 11)];
         }
         return key;
     }
     create(callback: Function) {
         let key = "";
         try {
-            let _window:any = window;
-            if(!_window.NukeJSCore) {
+            let _window: any = window;
+            if (!_window.NukeJSCore) {
                 _window.NukeJSCore = new NukeJSCoreStructDefine;
             }
             key = 'CALLBACK_' + this.makeKey();
             _window.NukeJSCore.callback[key] = callback;
             window = _window;
-        }catch(e) {
+        } catch (e) {
         }
-        return `NukeJSCore.callback.${key}()`;
+        return `NukeJSCore.callback.${key}(this)`;
     }
 }
 class NukeJSCoreStructDefine {
-    status : any = {}
+    status: any = {}
     callback: any = {}
 }
 class NukeJSCoreUseState {
-    constructor(private key:string="",private value:string="") {
-        let _window : any = window;
-        if(_window.NukeJSCore && _window.NukeJSCore.status && _window.NukeJSCore.status[this.key]) {
+    constructor(private key: string = "", private value: string = "") {
+        let _window: any = window;
+        if (_window.NukeJSCore && _window.NukeJSCore.status && _window.NukeJSCore.status[this.key]) {
             this.value = _window.NukeJSCore.status[this.key];
-        } 
+        }
     }
-    set(value:any = "") {
+    set(value: any = "") {
         this.value = value;
         try {
-            let _window:any = window;
-            if(!_window.NukeJSCore) {
+            let _window: any = window;
+            if (!_window.NukeJSCore) {
                 _window.NukeJSCore = new NukeJSCoreStructDefine;
             }
-            _window.NukeJSCore.status[this.key] =  this.value;
+            _window.NukeJSCore.status[this.key] = this.value;
             _window.nukepage.render();
             window = _window;
-        }catch(e) {
+        } catch (e) {
 
         }
     }
-    get(){
+    get() {
         return this.value;
     }
 }
 class NukeJSDoom extends Document {
-    constructor(){
+    constructor() {
         super();
-        
+
     }
-    build(rootId:string = "",html : string=""){
-        this.add(rootId,html);
+    build(rootId: string = "", html: string = "") {
+        this.add(rootId, html);
     }
-    private add(rootId:string = "",html : string=""){
-        if(document.getElementById(rootId)) {
-            let root = window.document.getElementById(rootId);
-            if(root !== null) {
-                root.innerHTML = html;
+    private add(rootId: string = "", html: string = "") {
+        if (document.getElementById(rootId)) {
+            let rootOld = window.document.getElementById(rootId);
+            if(!rootOld) {
+                return;
             }
+            return rootOld.innerHTML = html;
         }
     }
 }
 class NukeJSAdapter implements NukeJSCoreInterface {
-    hooks : {
-        useState : Function
+    hooks: {
+        useState: Function
     } = {
-        useState : (key:string="",defaultValue:string = "") => {
-            return new NukeJSCoreUseState(key,defaultValue)
+            useState: (key: string = "", defaultValue: string = "") => {
+                return new NukeJSCoreUseState(key, defaultValue)
+            }
         }
-    }
     doom: NukeJSDoom = new NukeJSDoom;
     clientCallback: clientCallback = new clientCallback;
 }
 
-export default class _NukeJSCore extends NukeJSAdapter {}
+export default class _NukeJSCore extends NukeJSAdapter { }
